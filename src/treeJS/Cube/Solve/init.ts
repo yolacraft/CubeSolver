049039c19ae4ce331bb63c3c24fa3bab028acbfc
yolacraft1604 <rubenbertram1604@gmail.com>
2025-12-07 18:56:59 +0100

    upload

--- src/treeJS/Cube/Solve/init.ts
+++ src/treeJS/Cube/Solve/init.ts
@@ -44,7 +44,7 @@ export const initSolve = () => {
 };
 
 async function fetchCubeSolution(cubeState: string): Promise<string> {
-    const url = 'http://45.93.249.131:8000/';
+    const url = 'http://45.93.249.131:7777/';
 
 
 
