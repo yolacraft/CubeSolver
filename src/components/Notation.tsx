4c8bb6b97ea299df7f6db7f5fa266b23b6979a12
yolacraft1604 <rubenbertram1604@gmail.com>
2025-12-08 18:17:15 +0100

    upload

--- src/components/Notation.tsx
+++ src/components/Notation.tsx
@@ -17,7 +17,7 @@ export const CubeNotation: React.FC<CubeProps> = ({ action, state, idx, cubeStat
         setToCubestate(cubeState);
         prevMoves.forEach(move => performMove(getNotationFromNumber(move)));
 
-        setIdx(idx+1);
+        setIdx(idx);
     }
 
     let textColor = "text-white";
